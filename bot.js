11b549e52527626c6008bdf790bec1f278e8556b
miyada-test <unity.hiroki.miyada.test@gmail.com>
2017-05-30 11:05:38 +0900

    たぶんなおった

--- bot.js
+++ bot.js
@@ -68,7 +68,7 @@ controller.hears(['ちん'], 'direct_message,direct_mention,mention', function (
 // 質問形式の会話
 //=========================================================
 
-controller.hears(['ラーメン'], 'direct_message,direct_mention,mention', function (bot, message) {
+controller.hears(['好きな女性のタイプ'], 'direct_message,direct_mention,mention', function (bot, message) {
 
     bot.reply(message, ':ramen:いいですよね:grin:');
 
@@ -76,21 +76,21 @@ controller.hears(['ラーメン'], 'direct_message,direct_mention,mention', func
     bot.startConversation(message, function (err, convo) {
 
         // convo.ask() で質問をします。
-        convo.ask('私が何味が好きか当ててみてください！', [
+        convo.ask('私の好きな女性のタイプを当ててみてください', [
             {
-                pattern: '醤油', // マッチさせる単語
+                pattern: 'すず', // マッチさせる単語
                 callback: function (response, convo) {
 
                     // ▼ マッチした時の処理 ▼
 
-                    convo.say('正解！:ok_woman:\n醤油！これぞ王道！:+1:'); // convo.say()で発言をします。
+                    convo.say('正解！:ok_woman:\nペロペロしたい（ハァハァal:+1:'); // convo.say()で発言をします。
                     convo.next(); // convo.next()で、会話を次に進めます。通常は、会話が終了します。
                 }
             },
             {
-                pattern: '味噌',
+                pattern: '先輩',
                 callback: function (response, convo) {
-                    convo.say('正解！:ok_woman:\n寒いと味噌たべたくなります！:+1:');
+                    convo.say('正解！:ok_woman:\nイキスギィ！！！:+1:');
                     convo.next();
                 }
             },
@@ -100,7 +100,7 @@ controller.hears(['ラーメン'], 'direct_message,direct_mention,mention', func
 
                     // ▼ どのパターンにもマッチしない時の処理 ▼
 
-                    convo.say('うーん、おしいです！:no_good:');
+                    convo.say('うーん、それもあり:no_good:');
                     convo.repeat(); // convo.repeat()で、質問を繰り返します。
                     convo.next(); // 会話を次に進めます。この場合、最初の質問にも戻ります。
                 }
@@ -111,53 +111,6 @@ controller.hears(['ラーメン'], 'direct_message,direct_mention,mention', func
 
 });
  
- 
- controller.hears(['体位'], 'direct_message,direct_mention,mention', function (bot, message) {
-
-    bot.reply(message, ':ramen:いいですよね:grin:');
-
-    // 会話を開始します。
-    bot.startConversation(message, function (err, convo) {
-
-        // convo.ask() で質問をします。
-        convo.ask('私がどの体位が好きか当ててみてください！', [
-            {
-                pattern: '騎乗位', // マッチさせる単語
-                callback: function (response, convo) {
-
-                    // ▼ マッチした時の処理 ▼
-
-                    convo.say('正解！:ok_woman:\n騎乗位！これぞ王道！:+1:'); // convo.say()で発言をします。
-                    convo.next(); // convo.next()で、会話を次に進めます。通常は、会話が終了します。
-                }
-            },
-            {
-                pattern: 'バック',
-                callback: function (response, convo) {
-                    convo.say('正解！:ok_woman:\n逆に後ろから突かれたい//////:+1:');
-                    convo.next();
-                }
-            },
-            {
-                default: true,
-                callback: function (response, convo) {
-
-                    // ▼ どのパターンにもマッチしない時の処理 ▼
-
-                    convo.say('ハァハァ:no_good:');
-                    convo.repeat(); // convo.repeat()で、質問を繰り返します。
-                    convo.next(); // 会話を次に進めます。この場合、最初の質問にも戻ります。
-                }
-            }
-        ]);
-
-    })
-
-});
-
-
-
-
 //=========================================================
 // 絵文字リアクション
 //=========================================================
@@ -242,7 +195,7 @@ controller.hears(['(.*)って呼んで'], 'direct_message,direct_mention,mention
 
             // ▼ 保存完了後の処理▼
 
-            bot.reply(message, 'あなたのお名前は *' + user_info.name + '* さんですね！覚えました！');
+            bot.reply(message, 'あなたのお名前は *' + user_info.name + '* さんですね！ちんこ');
 
         });
 
